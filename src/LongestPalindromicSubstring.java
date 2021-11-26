4bd781886e099e9e0ad9336e406f1ae6ef179080
Mrinal Ganesh <mrinalganesh@gmail.com>
2021-11-26 20:36:59 +0800

    Update Palindromic Strings

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,7 +1,77 @@
 public class LongestPalindromicSubstring {
-    public String longestPalindrome(String s) {
-        return "";
+    public static void main(String[] args) {
+
+        LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+        System.out.println(l.longestPalindrome("dabbaad"));
+        System.out.println(l.longestPalindrome("acbbdc"));
+        System.out.println(l.longestPalindrome("babad"));
+        System.out.println(l.longestPalindrome("ccc"));
 
     }
+
+
+    public String longestPalindrome(String s) {
+        String longest = "";
+        for(int i = 0 ; i < s.length(); i++) {
+
+            String even = longestPalindrome(s, i, true);
+            String odd = longestPalindrome(s, i, false);
+
+            String longer = even.length() > odd.length()
+                    ? even
+                    : odd;
+
+            if(longer.length() > longest.length()) {
+                longest = longer;
+            }
+        }
+
+        return longest;
+
+    }
+
+    public String longestPalindrome(String s, int middle, boolean even) {
+        int left, right;
+        String longestPalindrome;
+
+        if(!even) {
+            left = middle;
+            right = middle;
+
+            while(s.charAt(left) == s.charAt(right)) {
+                if(left > 0 && right < s.length() - 1) {
+                    left--;
+                    right++;
+                } else {
+                    left--;
+                    right++;
+                    break;
+                }
+            }
+        } else {
+            if(middle == s.length() - 1) {
+                return s.substring(middle);
+            }
+
+            left = middle;
+            right = middle + 1;
+
+            while (s.charAt(left) == s.charAt(right)) {
+                if(left > 0 && right < s.length() - 1) {
+                    left--;
+                    right++;
+                } else {
+                    left--;
+                    right++;
+                    break;
+                }
+
+            }
+        }
+        return s.substring(Integer.min(left + 1, right), right);
+    }
+
+
+
     
 }
